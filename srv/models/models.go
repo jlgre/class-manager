fa0fcb71fbd183b820b9c67ea011341139cbb039
Luke Green <lukeg496@gmail.com>
2021-12-12 19:56:00 -0600

    define models

--- srv/models/models.go
+++ srv/models/models.go
@@ -1,14 +1,39 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
+	ID           uint
+	FirstName    string
+	LastName     string
+	UserName     string
+	Password     string
+	GlobalAdmin  bool
+	Classes      []Class `gorm:"many2many:user_classes;"`
+	AdminClasses []Class `gorm:"many2many:user_admin_classes;"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
+
+type Class struct {
+	gorm.Model
 	ID        uint
-	FirstName string
-	LastName  string
-	UserName  string
-	Password  string
+	Name      string
+	Code      string
+	Notes     []Note
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Note struct {
+	ID        uint
+	Name      string
+	Markdown  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
